af15ac015d46a63c19b1213495d5b091f82c7958
darq <prototypid@gmail.com>
2023-12-10 07:38:00 +0530

    feat: Add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,30 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
 
 export default function Home() {
   return (
     <main className="flex h-full justify-center items-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
       <div className="space-y-6 text-center">
-        <h1 className="text-6xl font-semibold text-white drop-shadow-md">
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            font.className
+          )}
+        >
           🔐 Auth
         </h1>
         <p className="text-white text-lg">A simple authentication service</p>
+        <div>
+          <Button variant="secondary" size="lg">
+            Sign in
+          </Button>
+        </div>
       </div>
     </main>
   );
